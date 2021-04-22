6452a782c997a4d35d6cbe9b98db3051d53a4007
leo <leo.buono@ontariotechu.net>
2021-04-22 18:42:02 -0400

    Ok third time's the charm

--- samples/INFR-2350U/Week 6 Starter/src/Graphics/Post/DepthOfField.h
+++ samples/INFR-2350U/Week 6 Starter/src/Graphics/Post/DepthOfField.h
@@ -3,7 +3,7 @@
 #include "Graphics/Post/PostEffect.h" 
 #include <GLM/glm.hpp>
 
-class BloomEffect : public PostEffect
+class DepthOfFieldEffect : public PostEffect
 {
 public:
 	//Initializes framebuffer
@@ -22,9 +22,11 @@ public:
 
 private:
 	
-	int _passes = 10;
+	int _passes = 100;
 	float _threshold = 0.1f;
 	//float _threshold = 0.1f;
+	glm::vec4 _lightPos = glm::vec4(0);
+	glm::vec4 _colour = glm::vec4(0);
 	glm::vec2 _pixelSize;
 	float _blurValues[5] = { 0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216 }; 
 
